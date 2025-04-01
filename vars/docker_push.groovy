d27d50ff87a8fefde72ac91fd738fd86db22b8aa
Pardhu Alapati <80155814+pardhualapati@users.noreply.github.com>
2025-04-01 11:28:51 -0500

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -5,7 +5,7 @@ def call(String credId, String ImageName ){
                     usernameVariable: "dockerHubuser"
                 )]){    
                     sh "docker login -u ${env.dockerHubuser} -p ${env.dockerHubpass}"
-                    sh "docker image tag ${ImageName} ${env.dockerHubuser}/${Imagename}"
-                    sh "docker push ${env.dockerHubuser}/${Imagename}:latest"
+                    sh "docker image tag ${ImageName} ${env.dockerHubuser}/${ImageName}"
+                    sh "docker push ${env.dockerHubuser}/${ImageName}:latest"
                 }
 }
